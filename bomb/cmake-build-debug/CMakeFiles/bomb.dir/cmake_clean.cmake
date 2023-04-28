fef3e58871dff2304e6e5308174c89b6f47791ef
xtx2022 <1793071674@qq.com>
2023-04-29 01:07:08 +0800

    Try to Compile

---
+++ bomb/cmake-build-debug/CMakeFiles/bomb.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bomb.dir/bomb.c.o"
+  "CMakeFiles/bomb.dir/bomb.c.o.d"
+  "bomb"
+  "bomb.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/bomb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ bomb/cmake-build-debug/CMakeFiles/bomb.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for bomb.
+# This may be replaced when dependencies are built.
